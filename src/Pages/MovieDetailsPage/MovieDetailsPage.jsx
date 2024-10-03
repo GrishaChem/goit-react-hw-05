d9d3f8b5f62d8e55645ce4f1e6a4a2065048dc98
GrishaChem <Hryhoryy.chemerys@gmail.com>
2024-10-03 10:10:42 +0300

    qwesdzxc

--- src/Pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/Pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams, NavLink, Outlet } from "react-router-dom";
 import { fetchMovieById } from "../../services";
+import s from "../MovieDetailsPage/MovieDetailsPage.module.css";
 
 const MovieDetailsPage = () => {
   const { movieId } = useParams();
@@ -11,6 +12,7 @@ const MovieDetailsPage = () => {
     const getData = async () => {
       const data = await fetchMovieById(movieId);
       setMovie(data);
+      console.log(data);
     };
     getData();
   }, [movieId]);
@@ -19,10 +21,17 @@ const MovieDetailsPage = () => {
 
   return (
     <div>
-      <h2>{movie.title}</h2>
-      <p>{movie.description}</p>
-      <hr />
-      <div>
+      <div className={s.container}>
+        <img
+          src={"https://image.tmdb.org/t/p/w500/" + movie.backdrop_path}
+          alt=""
+        />
+        <div>
+          <h2>{movie.title}</h2>
+          <p>{"User score: " + 10 * movie.vote_average + "%"}</p>
+        </div>
+      </div>
+      <div className={s.Nav}>
         <NavLink to="info">Info</NavLink>
         <NavLink to="reviews">Reviews</NavLink>
       </div>
